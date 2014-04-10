41e04f0551654fc869115a28ab392e260aaa53bc
lauraconwill <lauraconwill@gmail.com>
2014-04-10 18:55:38 -0400

    added active class for navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 module ApplicationHelper
-  current_page? histogram_path
+
+  def nav_link(link_text, link_path)
+    class_name = current_page?(link_path) ? 'active' : ''
+
+    content_tag(:li, :class => class_name) do
+      link_to link_text, link_path
+    end
+  end
+
 end
+
+
