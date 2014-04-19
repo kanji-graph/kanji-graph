43bd0f7087bdfcad35bccafca8f72b7278a9e20b
Wang <eugene.wang.77@gmail.com>
2014-04-19 14:49:56 -0400

    edits and comments

--- app/assets/javascripts/visualizations/histogram.js
+++ app/assets/javascripts/visualizations/histogram.js
@@ -8,22 +8,22 @@ d3.json("/surnames/histogram", function(error, json) {
 
 
 function draw_histogram(json) {
-  var data = json;
-
   var x = d3.scale.linear()
       .domain([0, max_property_value(json, 'value')])     //d3.max(+data.value)
       .range([0, 580]);
 
   d3.select(".chart")
     .selectAll("div")
-      .data(data)
+      .data(json)
     .enter().append("div")
       .style("width", function(d) { console.log(d.value); return x(d.value) + "px";  })
       .html(function(d) { return d.kanji + "<span>" + d.value + "</span>"; });
   }
 });
 
-
+// Might be useful to use built-in collection functions from Underscore.js here
+// For example, you should be able to write the same for loop as: _.max(
+// object_array, function(item) { item[property] }), or something like that
 function max_property_value(object_array, property) {
   var curr_max = 0;
   for (i = 0; i < object_array.length; i++) {
